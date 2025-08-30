8b9349c9d9af8606bee8e6057c842efaac24a409
Lionel Wijaya <lionel_cw@proton.me>
2025-08-30 21:12:04 +0700

    refactor: remove file extension checker

--- src/multer.ts
+++ src/multer.ts
@@ -12,37 +12,12 @@ const storage = multer.diskStorage({
     },
 });
 
-const ALLOWED_EXTENTIONS = [
-    ".png",
-    ".pdf",
-    ".docx",
-    ".doc",
-    ".xlsx",
-    ".pptx",
-    ".ppt",
-    ".odt",
-    ".odp",
-    ".ods",
-    ".txt",
-    ".jpg",
-    ".jpeg",
-    ".webp",
-];
-
 export const uploadFile = multer({
     storage,
     limits: {
-        // byte,
-        // 1kb = approx 1000byte (real case its 1024byte)
         fileSize: 10 * 1024 * 1024, // 10 MB
     },
     fileFilter: (req, file, callback) => {
-        const ext = path.extname(file.originalname);
-
-        if (ALLOWED_EXTENTIONS.every((e) => e !== ext)) {
-            return callback(new Error("File is not supported!"));
-        }
-
         callback(null, true);
     },
 });
